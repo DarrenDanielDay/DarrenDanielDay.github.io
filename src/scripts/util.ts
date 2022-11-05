4b49fa82f8813db6e064d809175ba3223b1b5f1f
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-11-05 23:02:03 +0800

    refactor(global): optimised static resources

--- src/scripts/util.ts
+++ src/scripts/util.ts
@@ -1,7 +1,23 @@
+import type { AstroGlobal } from "astro";
+
 export interface MyFrontMatter {
   title: string;
   pubDate: string;
   description: string;
   author: string;
   tags: string[];
+}
+export function query(Astro: AstroGlobal) {
+  return {
+    assets(path: string) {
+      const url = `${Astro.url.origin}/assets/${path.replace(/^\//, "")}`;
+      return {
+        async text() {
+          const response = await fetch(url);
+          const text = await response.text();
+          return text;
+        },
+      };
+    },
+  };
 }
