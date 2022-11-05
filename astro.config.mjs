8f4c0cf43fdf259a94768bc03e68829b510536af
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-11-05 13:25:44 +0800

    feat: add blogs and first blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx({
+    extendPlugins: 'astroDefaults',
+  })]
+});
---
+++ src/scripts/util.ts
@@ -0,0 +1,7 @@
+export interface MyFrontMatter {
+  title: string;
+  pubDate: string;
+  description: string;
+  author: string;
+  tags: string[];
+}
