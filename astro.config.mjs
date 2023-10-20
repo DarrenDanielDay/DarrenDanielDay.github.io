2b6192e3b59838188f1372588593120d8ea0afed
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2023-10-21 01:55:30 +0800

    migrate(global): `astro`: `v1` -> `v3`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     mdx({
-      extendPlugins: "astroDefaults",
     }),
-    image(),
   ],
 });
